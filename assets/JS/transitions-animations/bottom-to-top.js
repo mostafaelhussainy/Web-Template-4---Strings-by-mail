f626696b20c7ec3b9b9653792e9540ce25852870
mostafaelhussainy <ayman.nagy@lexmodo.com>
2023-03-10 15:55:42 +0200

    category UI

--- assets/JS/transitions-animations/bottom-to-top.js
+++ assets/JS/transitions-animations/bottom-to-top.js
@@ -1,10 +1,12 @@
 const sections = document.querySelectorAll('.out-view');
 const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+  entries.forEach((entry, index) => {
     const section = entry.target;
     if (entry.isIntersecting) {
-      section.classList.add('in-view', 'animated');
-      observer.unobserve(section);
+      setTimeout(() => {
+        section.classList.add('in-view', 'animated');
+        observer.unobserve(section);
+      }, index * 50);
     } else {
       section.classList.remove('in-view', 'animated');
     }
