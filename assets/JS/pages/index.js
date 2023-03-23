f9085f8cc3ee2ce57759e7f6e5a359536660d190
mostafaelhussainy <mostafaelhussainy22@gmail.com>
2023-03-23 11:47:57 +0200

    news letter

--- assets/JS/pages/index.js
+++ assets/JS/pages/index.js
@@ -35,9 +35,24 @@ var swiper = new Swiper(".testimonialsSlider", {
 // N E W S    L E T T E R
 
 const closeNewsLetterBtn = document.querySelector(".close-news-letter");
-closeNewsLetterBtn.addEventListener("click", () => {
-  const newsLetterPopUp = document.querySelector("#news-letter");
-  newsLetterPopUp.classList.add("closeNewsLetter")
-})
+const newsLetterPopUp = document.querySelector("#news-letter");
+const newsLetterContainer = document.querySelector(".news-letter-container");
+const subscribeBtn = document.querySelector("#subscribe-btn");
+const subscribeInput = newsLetterPopUp.querySelector("input");
 
+closeNewsLetterBtn.addEventListener("click", () => {
+  newsLetterPopUp.classList.add("closeNewsLetter")
+});
+
+const subscribeToNewsLetter = (e) => {
+  e.preventDefault()
+  if (subscribeInput.validity.valid) {
+    newsLetterContainer.innerHTML = '<h6>Thanks for your subscription!</h6>';
+    setTimeout(() => newsLetterPopUp.classList.add("closeNewsLetter"), 1500)
+  } else {
+    newsLetterPopUp.querySelector(".invalid").style.display = "flex";
+  }
+}
+
+subscribeBtn.addEventListener("click", subscribeToNewsLetter);
 
